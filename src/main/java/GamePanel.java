7e63d742acb17d4212dba0b16ece4baea6b594ee
kakarot <harelasaraf@gmail.com>
2024-11-09 00:30:36 -0800

    Adding game Thread loop

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -3,7 +3,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
 	// SCREEN SETTINGS
 	final int originalTileSize = 16; // 16x16 tile
@@ -15,10 +15,27 @@ public class GamePanel extends JPanel {
 	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
 	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+	Thread gameThread;
+
 	public GamePanel() {
 
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+	}
+
+	public void startGameThread() {
+
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	@Override
+	public void run() {
+
+		while (gameThread != null) {
+
+			// System.out.println("This game loop is running");
+		}
 	}
 }
