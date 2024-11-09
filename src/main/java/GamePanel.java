6b2b31555e0ef46961efd70ac21a6b3cadf6fd95
kakarot <harelasaraf@gmail.com>
2024-11-09 00:13:34 -0800

    Debugged path to jar file

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -1,5 +1,3 @@
-package main;
-
 import java.awt.Color;
 import java.awt.Dimension;
 
@@ -16,10 +14,9 @@ public class GamePanel extends JPanel {
 	final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
 	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
-	
 
 	public GamePanel() {
-		
+
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
