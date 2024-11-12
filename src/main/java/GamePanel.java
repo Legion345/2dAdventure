4a7f74fe0fb96219d94bea002918addbf643200f
kakarot <harelasaraf@gmail.com>
2024-11-12 01:25:52 -0800

    Updated Character movement loop with Delta Method

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -43,33 +43,68 @@ public class GamePanel extends JPanel implements Runnable {
 		gameThread.start();
 	}
 
-	@Override
+	// @Override
+	// public void run() {
+	// double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+	// double nextDrawTime = System.nanoTime() + drawInterval;
+
+	// while (gameThread != null) {
+
+	// update();
+
+	// repaint();
+
+	// try {
+
+	// double remainingTime = nextDrawTime - System.nanoTime();
+	// remainingTime = remainingTime / 1000000;
+
+	// if (remainingTime < 0) {
+	// remainingTime = 0;
+	// }
+
+	// Thread.sleep((long) remainingTime);
+
+	// nextDrawTime += drawInterval;
+
+	// nextDrawTime += drawInterval;
+	// } catch (InterruptedException e) {
+	// e.printStackTrace();
+	// }
+
+	// }
+	// }
+
+	// Delta/Accumulator Method
 	public void run() {
-		double drawInterval = 1000000000 / FPS; // 0.01666 seconds
-		double nextDrawTime = System.nanoTime() + drawInterval;
+
+		double drawInterval = 1000000000 / FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
 
 		while (gameThread != null) {
 
-			update();
+			currentTime = System.nanoTime();
 
-			repaint();
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
 
-			try {
-
-				double remainingTime = nextDrawTime - System.nanoTime();
-				remainingTime = remainingTime / 1000000;
-
-				if (remainingTime < 0) {
-					remainingTime = 0;
-				}
-
-				Thread.sleep((long) remainingTime);
-
-				nextDrawTime += drawInterval;
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			if (delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
 			}
 
+			if (timer >= 1000000000) {
+				System.out.println("FPS:" + drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
 		}
 	}
 
